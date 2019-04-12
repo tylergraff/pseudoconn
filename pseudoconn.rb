5805d40a14276422ca0565fb5a46bc996116d744
tyler <tyler@notostech.com>
2019-04-12 13:50:10 -0400

    add ecs6 support

--- pseudoconn.rb
+++ pseudoconn.rb
@@ -1,6 +1,31 @@
 #!/usr/bin/env ruby
 # encoding: ASCII-8BIT
 
+# Copied from https://github.com/kedji/pseudoconn/
+
+#  commit a63ecea2cf293f79628c10804d0b2d261c1a5c99 (Apr 5, 2019)
+#
+# Copyright (c) 2010-2016 Eric Davidson
+# ** Pseudoconn is governed by the MIT License **
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 # This class provides a simple scripting interface to simulating client/server
 # network traffic over TCP or UDP.
 
